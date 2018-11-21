ee6cec482130d8b09741e846b6ea8ffbbd594624
Jose Naranjo <naranjja@gmail.com>
2018-11-21 18:44:48 -0500

    :sparkles: Add stream test

---
+++ src/tests/stream.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+camera_index = 1  # 0: built-in, 1: external
+cap = cv2.VideoCapture(camera_index)
+
+while True:
+    is_correctly_setup, frame = cap.read()  # get next frame
+    cv2.imshow("frame", frame)  # show frame on popup window
+    if cv2.waitKey(1) & 0xFF == ord("q"):  # if user wants to close
+        break
+
+cap.release()  # kill capture
+cv2.destroyAllWindows()  # kill windows
